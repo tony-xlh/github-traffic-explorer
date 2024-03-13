a850ba2fd3ac9d1900ec5ed94b2ab5fec1517638
xulihang <xulihanghai@163.com>
2024-03-13 10:43:56 +0800

    import ant-d

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './assets/main.css'
-
+import Antd from 'ant-design-vue';
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'ant-design-vue/dist/reset.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Antd).mount('#app');
